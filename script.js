4a4daded2621cf90e0d6fc766400fb654f553b73
agarcia586 <agarcia586@cps.edu>
2024-12-02 17:22:06 +0000

    declineopp

--- script.js
+++ script.js
@@ -1,24 +1,60 @@
+//beggining options
 let decline =document.querySelector('.decline');
-let accept =document.querySelector('.right');
-let decAcc =document.querySelector('.decAcc');
+let accept =document.querySelector('.accept');
+let aord =document.querySelector('.aord');
+let party =document.querySelector('.party');
 let text =document.querySelector('p');
+// 
+let death =document.querySelector('.death');
+//decline options
+let calling=document.querySelector('.calling');
+let found=document.querySelector('.found');
+let run=document.querySelector('.run');
+let cback=document.querySelector('.cback');
+let end=document.querySelector('.end');
 
-let pizza =document.querySelector('.pizza');
-pizza.style.display='none';
-
+//starting screen
+party.style.display='none';
+calling.style.display='none';
+found.style.display='none';
+run.style.display='none';
+cback.style.display='none';
+end.style.display='none';
 
 decline.addEventListener('click',function() {
 
-    decAcc.style.display="none";
+    aord.style.display="none";
     decline.style.display="none";
     accept.style.display="none";
 
-    pizza.style.display='block';
+    party.style.display='block';
+    cback.style.display='block';
 
-    text.innerHTML="click to eat";
+    text.innerHTML="Your lights start to flicker, And you see a scary figure in the alley. What do you do? Doublick click to run, or click call back button to call back";
 });
+//call back
+cback.addEventListener('click',function() {
+    aord.style.display="none";
+    decline.style.display="none";
+    accept.style.display="none";
+    party.style.display='none';
+    cback.style.display='none';
+    calling.style.display='block';
 
-pizza.addEventListener('dblclick',function() {
+    text.innerHTML="You begging to regret declining, freakbob, and in a rush you hastily call him back, click end button to see how the story ends ";
 
-    text.innerHTML="yummmm";
+});
+//runing away
+party.addEventListener("dblclick", function(){
+    party.style.display ="none";
+     text.innerHTML = "You hear fast footsteps approach you, you get scared and things get scary you decide to run, hover to see how the story ends"
+     run.style.display ="block";
+     cback.style.display='none';
+     end.style.display='block';
+ });
+ end.addEventListener('click',function() {
+    run.style.display='none';
+    text.innerHTML = "Freakbob catches you, you are no where to be found and you are now his forever";
+    found.style.display ="block";
+    end.style.display='none';
 });
