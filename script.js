4a8b414b6b31aba516fc82b0dd01d7970488de38
agarcia586 <agarcia586@cps.edu>
2024-12-09 04:38:30 +0000

    finished before making cute

--- script.js
+++ script.js
@@ -23,7 +23,11 @@ let h2= document.querySelector('hide')
 let gang= document.querySelector('.gang')
 let stay=document.querySelector('.stay')
 let mis=document.querySelector('.mis')
-
+let stalk=document.querySelector('.stalk')
+let finish=document.querySelector('.finish')
+let fp=document.querySelector('.fp')
+let tasting=document.querySelector('.tasting')
+let human=document.querySelector('.human')
 //starting screen
 party.style.display='none';
 calling.style.display='none';
@@ -36,7 +40,11 @@ goofy.style.display='none';
 gang.style.display='none';
 mis.style.display='none';
 stay.style.display='none';
-
+stalk.style.display='none';
+finish.style.display='none';
+fp.style.display='none';
+tasting.style.display='none';
+human.style.display='none';
 
 //decline path
 decline.addEventListener('click',function() {
@@ -93,3 +101,41 @@ accept.addEventListener('click',function() {
     mis.style.display='block';
     stay.style.display='block';
 });
+//stay
+stay.addEventListener('click',function() {
+    gang.style.display='none';
+    stalk.style.display='block';
+    text.innerHTML="At the party, you glance out the window and see Freakbob’s pale face pressed against the glass, eyes locked on you. The laughter around you fades as his stare burns through the glass, making your skin crawl., click the 'finish' button to end.";
+    mis.style.display='none';
+    stay.style.display='none';
+    finish.style.display='block';
+});
+finish.addEventListener('click',function() {
+    stalk.style.display='none';
+    fp.style.display='block';
+    text.innerHTML="You turn around, and suddenly, everyone at the party morphs into SpongeBob, their eyes wild with hunger. Without warning, they lunge at you, their laughter twisted and manic.";
+    finish.style.display='none';
+});
+//go and see him
+stay.addEventListener('click',function() {
+    gang.style.display='none';
+    stalk.style.display='block';
+    text.innerHTML="At the party, you glance out the window and see Freakbob’s pale face pressed against the glass, eyes locked on you. The laughter around you fades as his stare burns through the glass, making your skin crawl., click the 'finish' button to end.";
+    mis.style.display='none';
+    stay.style.display='none';
+    finish.style.display='block';
+});
+//eat the burger
+mis.addEventListener('click',function() {
+    gang.style.display='none';
+    tasting.style.display='block';
+    text.innerHTML="He takes you to eat a new krabby patties You sit down at his kitchen table, and SpongeBob proudly presents a dozen bizarre-looking Krabby Patties. I’ve been experimenting with some new recipes... like this one with sea cucumber and barnacle bits. Don't worry, it's not too weird! He says. As he hands you a suspicious-looking patty, you can't shake the feeling that he's a bit too excited for you to try it. Trust me, it's totally edible! Double click to see how the story ends";
+    mis.style.display='none';
+    stay.style.display='none';
+});
+tasting.addEventListener("dblclick", function(){
+    tasting.style.display ="none";
+     text.innerHTML = "You take a bite of the burger, and something feels wrong. Looking closer, you realize it's not meat—it's human."
+     human.style.display ="block";
+     cback.style.display='none';
+ });
